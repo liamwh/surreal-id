bea79c1e35c27b9b933eac5ea3098914416c0148
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-09-22 16:22:09 +0200

    Loosen interface, additional proptests

--- src/new_id.rs
+++ src/new_id.rs
@@ -1,6 +1,5 @@
-use surrealdb::sql::Id;
-
 use crate::errors::IdError;
+use surrealdb::sql::Id;
 
 /// Defines a trait for creating a new ID with table name and inner ID.
 ///
@@ -14,46 +13,45 @@ pub trait NewId: Sized {
     const TABLE: &'static str;
 
     /// Creates a new instance of the implementing type by parsing and validating the given `id` string.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if the `id` string is empty, malformed, or doesn't match the expected table name.
-    fn new(id: &str) -> Result<Self, IdError> {
-        if id.is_empty() {
+    fn new<T: AsRef<str>>(id: T) -> Result<Self, IdError> {
+        let id_ref = id.as_ref();
+
+        let is_empty = id_ref.is_empty();
+        if is_empty {
             return Err(IdError::IdCannotBeEmpty);
         }
 
-        let mut split_at_colon = id.splitn(2, ':');
-        let table_part = split_at_colon
-            .next()
-            .ok_or(IdError::InvalidIdFormat("Missing table part".to_string()))?;
+        let mut split_at_colon = id_ref.splitn(2, ':');
+        let table_part = split_at_colon.next().unwrap_or(Self::TABLE);
+        let id_part = split_at_colon.next().unwrap_or(id_ref);
+        let start_bracket_count = id_ref.chars().filter(|&c| c == '⟨').count();
+        let end_bracket_count = id_ref.chars().filter(|&c| c == '⟩').count();
+        let is_valid_format = id_part.starts_with('⟨')
+            && id_part.ends_with('⟩')
+            && start_bracket_count == 1
+            && end_bracket_count == 1;
 
-        if let Some(id_part) = split_at_colon.next() {
-            if id_part.starts_with('⟨') && id_part.ends_with('⟩') {
-                let id_inner: String = id_part
-                    .chars()
-                    .skip(1)
-                    .take(id_part.chars().count() - 2)
-                    .collect();
-
-                if id_inner.is_empty() {
-                    return Err(IdError::IdCannotBeEmpty);
-                }
-
-                if table_part != Self::TABLE {
-                    return Err(IdError::InvalidTable(
-                        Self::TABLE.to_string(),
-                        table_part.to_string(),
-                    ));
-                }
-
-                return Ok(Self::from_inner_id(id_inner));
-            } else {
-                return Err(IdError::InvalidIdFormat(id.to_string()));
+        if is_valid_format {
+            let id_inner: String = id_part.chars().skip(1).take_while(|&c| c != '⟩').collect();
+            if id_inner.is_empty() {
+                return Err(IdError::IdCannotBeEmpty);
             }
+
+            if table_part != Self::TABLE {
+                return Err(IdError::InvalidTable(
+                    Self::TABLE.to_string(),
+                    table_part.to_string(),
+                ));
+            }
+
+            return Ok(Self::from_inner_id(id_inner));
         }
 
-        Ok(Self::from_inner_id(id.to_string()))
+        if id_ref.contains(':') {
+            return Err(IdError::InvalidIdFormat(id_ref.to_string()));
+        }
+
+        Ok(Self::from_inner_id(id_part.to_string()))
     }
 
     /// Constructs an instance from an inner ID of type `T`.
